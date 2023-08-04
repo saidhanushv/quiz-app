c8cfb93eb904b6536845ac334066b6bcfdd8e787
Sai Dhanush V <saidhanushv@gmail.com>
2023-08-04 12:11:28 +0530

    Initial commit

---
+++ ui.py
@@ -0,0 +1,75 @@
+from tkinter import *
+
+import quiz_brain
+from quiz_brain import QuizBrain
+
+THEME_COLOR = "#375362"
+
+
+class QuizInterface:
+    def __init__(self, quiz_brain: QuizBrain):  # telling the program quiz_brain must be of the type QuizBrain //class
+        self.quiz = quiz_brain
+
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(bg=THEME_COLOR, padx=20, pady=20)
+
+        self.score_label = Label(text="Score: 0")
+        self.score_label.config(borderwidth=0, border=False, bg=THEME_COLOR, fg="white", pady=20)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bd=0, borderwidth=0, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="Some Question Text",
+            fill=THEME_COLOR,
+            font=("Ariel", 20, "italic")
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        correct = PhotoImage(file="images/true.png")
+        wrong = PhotoImage(file="images/false.png")
+
+        self.correct_btn = Button(image=correct, command=self.true_pressed)
+        self.correct_btn.grid(row=2, column=0)
+
+        self.wrong_btn = Button(image=wrong, command=self.false_pressed)
+        self.wrong_btn.grid(row=2, column=1)
+
+        self.next_question()
+
+        self.window.mainloop()
+
+    def next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've Reached The End Of The Quiz.")
+            self.correct_btn.config(state="disabled")
+            self.wrong_btn.config(state="disabled")
+            self.quiz.score += 1
+
+    def true_pressed(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+
+    def false_pressed(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+
+
+
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+
+        self.window.after(1000, self.next_question)
